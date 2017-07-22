9aab52998b74bdb7fbc2a8ce1c200211e1858775
rjrahul12 <jainr7750@gmail.com>
2017-07-22 22:47:08 +0530

    Validation Added to Name

--- db/migrate/20170716182335_add_name_to_user.rb
+++ db/migrate/20170716182335_add_name_to_user.rb
@@ -1,6 +1,6 @@
 class AddNameToUser < ActiveRecord::Migration
   def change
-    add_column :users, :firstName, :string
-    add_column :users, :lastName, :string
+    add_column :users, :firstName, :string,null: false
+    add_column :users, :lastName, :string,null: false
   end
 end
